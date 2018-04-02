3e40ac10d50d0be80875371d298be2b3abf3ce27
Matthew Roseman <mroseman95@gmail.com>
2018-04-02 19:15:35 -0400

    added functionality to read in redirects from JSON and YAML
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -1,11 +1,18 @@
 package urlshort
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	yaml "gopkg.in/yaml.v2"
 )
+
+// redirect represents a URL redirect. Path is the input and it maps to URL
+type redirect struct {
+	Path string
+	URL  string
+}
 
 // MapHandler will return an http.HandlerFunc (which also
 // implements http.Handler) that will attempt to map any
@@ -52,12 +59,6 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	return MapHandler(ymlMap, fallback), nil
 }
 
-// redirect represents a URL redirect. Path is the input and it maps to URL
-type redirect struct {
-	Path string
-	URL  string
-}
-
 // redirectYAMLToMap takes a slice of bytes representing a YAML document, and converts it to
 // a map of path strings to the url redirect string
 // YAML is expected to be in the format:
@@ -76,6 +77,58 @@ func redirectYAMLToMap(yml []byte) (ymlMap map[string]string, err error) {
 	// turn the slice of redirect types into a map
 	for _, r := range redirects {
 		ymlMap[r.Path] = r.URL
+	}
+
+	return
+}
+
+// JSONHandler will parse the provided JSON and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the JSON, then the
+// fallback http.Handler will be called instead.
+//
+// JSON is expected to be in the format:
+//
+//		[
+//			{path: /some-path, url: https://www.some-url.com/demo},
+//			...
+//		]
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func JSONHandler(jsonBytes []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	jsonMap, err := redirectJSONToMap(jsonBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// call MapHandler with this new map made from the YAML
+	return MapHandler(jsonMap, fallback), nil
+}
+
+// redirectJSONToMap takes a slice of bytes representing a JSON document, and converts it to
+// a map of path strings to the url redirect string
+// JSON is expected to be in the format:
+//
+//		[
+//			{path: /some-path, url: https://www.some-url.com/demo},
+//			...
+//		]
+func redirectJSONToMap(jsonBytes []byte) (jsonMap map[string]string, err error) {
+	var redirects []redirect
+	err = json.Unmarshal(jsonBytes, &redirects)
+	if err != nil {
+		return
+	}
+
+	jsonMap = make(map[string]string)
+	// turn the slice of redirect types into a map
+	for _, r := range redirects {
+		jsonMap[r.Path] = r.URL
 	}
 
 	return
--- urlshort/main/main.go
+++ urlshort/main/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/mroseman95/gophercises/urlshort"
 )
 
 func main() {
+	ymlFilePath := flag.String("yml", "redirects.yml", "filepath to the redirects YAML file")
+	jsonFilePath := flag.String("json", "redirects.json", "filepath to the redirects JSON file")
+	flag.Parse()
+
 	mux := defaultMux()
 
 	// Build the MapHandler using the mux as the fallback
@@ -17,20 +24,32 @@ func main() {
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
+	yaml, err := ioutil.ReadFile(*ymlFilePath)
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
 	// Build the YAMLHandler using the mapHandler as the
 	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
+
+	json, err := ioutil.ReadFile(*jsonFilePath)
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
+	// Build the JSONHandler using the YAMLHandler as the
+	// fallback
+	jsonHandler, err := urlshort.JSONHandler(json, yamlHandler)
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("Starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", jsonHandler)
 }
 
 func defaultMux() *http.ServeMux {
