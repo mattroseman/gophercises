92aded0487258d0162a5412b20d5669c3adbca79
Matthew Roseman <mroseman95@gmail.com>
2018-04-02 14:29:23 -0400

    quiz now starts a timer, and stops the quiz when timer runs out
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- quiz/main.go
+++ quiz/main.go
@@ -8,49 +8,99 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
+
+type problem struct {
+	question string
+	answer   string
+}
 
 func main() {
 	problemsFilePath := flag.String("csv", "problems.csv", "filepath to problems csv file")
+	limit := flag.Int("limit", 30, "time limit for each question in seconds")
 	flag.Parse()
 
-	// read in default csv file problems.csv
-	problemsCsv, err := os.Open(*problemsFilePath)
+	problems, err := readProblems(*problemsFilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	stdInReader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Press enter to start")
+	stdInReader.ReadString('\n')
+
+	// start timer
+	t := make(chan int)
+	go startTimer(*limit, t)
+
+	numQuestions := len(problems)
+	numCorrect := startQuiz(problems, t)
+
+	fmt.Println("")
+	fmt.Println("You scored", numCorrect, "out of", numQuestions)
+}
+
+func startTimer(limit int, t chan int) {
+	time.Sleep(time.Duration(limit) * time.Second)
+	t <- limit
+}
+
+func startQuiz(problems []problem, t chan int) int {
+	var numCorrect int
+
+	// start quiz
+	for i, problem := range problems {
+		// prompt the problem and read input
+		fmt.Print("#" + strconv.Itoa(i) + ": " + problem.question + " = ")
+
+		a := make(chan string)
+		go getAnswer(problem, a)
+
+		select {
+		case answer := <-a:
+			// knock off the newline
+			answer = answer[:len(answer)-1]
+
+			// compare input to answer and move on to next problem
+			if answer == problem.answer {
+				numCorrect++
+			}
+		case <-t:
+			return numCorrect
+		}
+	}
+
+	return numCorrect
+}
+
+func getAnswer(p problem, a chan string) {
+	stdInReader := bufio.NewReader(os.Stdin)
+	answer, _ := stdInReader.ReadString('\n')
+	a <- answer
+}
+
+func readProblems(problemsFilePath string) ([]problem, error) {
+	// read in csv file problems.csv
+	problemsCsv, err := os.Open(problemsFilePath)
 	if err != nil {
 		panic(err)
 	}
 	// iterate through and get problems and answers
 	csvReader := csv.NewReader(problemsCsv)
 
-	stdInReader := bufio.NewReader(os.Stdin)
-
-	var numCorrect int
-	var numQuestions int
+	var problems []problem
 
 	for {
 		record, err := csvReader.Read()
 		if err == io.EOF {
-			break
+			return problems, nil
 		}
 		if err != nil {
-			panic(err)
+			return nil, err
 		}
-		problem := record[0]
-		correctAnswer := record[1]
 
-		// prompt the problem and read input
-		fmt.Print("#" + strconv.Itoa(numQuestions) + ": " + problem + " = ")
-		answer, _ := stdInReader.ReadString('\n')
-		// knock off the newline
-		answer = answer[:len(answer)-1]
-
-		// compare input to answer and move on to next problem
-		numQuestions++
-		if answer == correctAnswer {
-			numCorrect++
-		}
+		problems = append(problems, problem{question: record[0], answer: record[1]})
 	}
-
-	fmt.Println("")
-	fmt.Println("You scored", numCorrect, "out of", numQuestions)
 }
